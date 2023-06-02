a181191d9f54a99757ddf620c95ff8b4129768ea
Mohnish <mohnish.a007@gmail.com>
2023-06-02 15:11:08 +0100

    The story structure using hieracary

--- src/components/buttons/Buttons.jsx
+++ src/components/buttons/Buttons.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function Buttons(props) {
-  const {color = 'bg-grey-700', children,  ...rest} = props
+  const {tailwind = 'bg-grey-700', children,  ...rest} = props
   return (
-    <button className={`${color}`}{...rest}>{children}</button>
+    <button className={`button ${tailwind}`}{...rest}>{children}</button>
   )
 }
 
